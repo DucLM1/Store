3c3d8ec902f2bd99c9f1d12e2e561d7983423ca2
DucLM1 <DucLM1@daivietgroup.net.vn>
2018-12-10 14:36:50 +0700

    add

---
+++ FirstProject.DomainLayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FirstProject.DomainLayer
+{
+    public class Class1
+    {
+    }
+}
---
+++ FirstProject.InfrastructureLayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FirstProject.InfrastructureLayer
+{
+    public class Class1
+    {
+    }
+}
